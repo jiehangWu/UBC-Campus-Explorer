e9d9436edbaf2e99d9aaf53893aedeff891f55ef
qju <qju@students.cs.ubc.ca>
2020-01-26 17:07:01 -0800

    changed import

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,10 +1,8 @@
 import Log from "../Util";
 import {IInsightFacade, InsightDataset, InsightDatasetKind, ResultTooLargeError} from "./IInsightFacade";
 import {InsightError, NotFoundError} from "./IInsightFacade";
-import { rejects } from "assert";
 import Query from "./Query/Query";
-import { addCourseDataset } from "./addDatasetHelper";
-import { promises } from "dns";
+import * as fs from "fs-extra";
 /**
  * This is the main programmatic entry point for the project.
  * Method documentation is in IInsightFacade
@@ -12,73 +10,18 @@ import { promises } from "dns";
  */
 export default class InsightFacade implements IInsightFacade {
     public idList: string[];
-    private datasets: InsightDataset[];
 
     constructor() {
         Log.trace("InsightFacadeImpl::init()");
         this.idList = [];
-        this.datasets = [];
     }
 
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-        if (this.validateId(id)) {
-            // check if id has already been added
-            if (this.idList.includes(id)) {
-                return Promise.reject(new InsightError("Database already contains the same id"));
-            }
-
-            if (kind === InsightDatasetKind.Courses) {
-                return addCourseDataset(id, content, kind)
-                    .then((result: any) => {
-                        this.idList.push(result[0]);
-                        this.datasets.push({
-                            id: result[0],
-                            kind: InsightDatasetKind.Courses,
-                            numRows: result[1],
-                        });
-                        return Promise.resolve(this.idList);
-                    })
-                    .catch((err: any) => {
-                        return Promise.reject(err);
-                    });
-            } else {
-                return Promise.reject(new InsightError("Invalid InsightDatasetKind"));
-            }
-
-        } else {
-            return Promise.reject(new InsightError("This id is invalid"));
-        }
+        return Promise.reject("Not implemented");
     }
 
     public removeDataset(id: string): Promise<string> {
-        if (!this.validateId(id)) {
-            return Promise.reject(new InsightError("This id is invalid"));
-        }
-
-        let path = "./data/" + id;
-        let fs = require("fs-extra");
-
-        if (this.idList.includes(id)) {
-            try {
-                fs.unlinkSync(path + "/" + id + ".json");
-                fs.removeSync(path);
-            } catch (err) {
-                return Promise.reject(new InsightError("Could not unlink dataset"));
-            }
-            this.idList = this.idList.filter((val: string) => {
-                if (val !== id) {
-                    return val;
-                }
-            });
-            this.datasets = this.datasets.filter((dataset: InsightDataset) => {
-                if (dataset.id !== id) {
-                    return dataset;
-                }
-            });
-            return Promise.resolve(id);
-        } else {
-            return Promise.reject(new NotFoundError("ID is not in the dataset"));
-        }
+        return Promise.reject("Not implemented");
     }
 
 //  todo: 1 if dataset not added? 2 put in dataset 3 display field 4 order 5 asterisk
@@ -98,7 +41,6 @@ export default class InsightFacade implements IInsightFacade {
             if (! this.idList.includes(queriedID)) {
                 throw new InsightError("Referenced data not added");
             }
-            const fs = require("fs");
             // https://stackoverflow.com/questions/10058814/get-data-from-fs-readfile
             let address = ("./data/courses/").concat(queriedID).concat(".json");
             let rawData = fs.readFileSync(address);
@@ -115,25 +57,7 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     public listDatasets(): Promise<InsightDataset[]> {
-        return Promise.resolve(this.datasets);
+        return Promise.reject("Not implemented");
     }
 
-    private validateId(id: string): boolean {
-        // check if id is null or undefined
-        if (typeof id === "undefined" || id === null) {
-            return false;
-        }
-        // "_" should not be included in id
-        if (id.includes("_")) {
-            return false;
-        }
-        // check if a string contains only whitespaces
-        for (let i = 0; i < id.length; i++) {
-            if (id.charAt(i) !== " ") {
-                return true;
-            }
-        }
-
-        return false;
-    }
 }
